2cd281028842c3056bff83515191024e82a9df9d
globalsmile <mohammedbadmus823@gmail.com>
2023-04-24 08:56:18 -0700

    Added: _print_format.c file and renamed function _write(char c) to _putchar(char c)

--- main.h
+++ main.h
@@ -1,19 +1,18 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 #include <stdarg.h>
-
 /**
-  * struct specifiers - Struct specifiers
-  * @specifier: The conversion specifier
-  * @f: The function pointer
-  */
+ * struct specifiers - Struct specifiers
+ * @specifier: The conversion specifier
+ * @f: The function pointer
+ */
 typedef struct specifiers
 {
 	char *specifier;
 	int (*f)(va_list args);
 } spc_dt;
 
-int _write(char c);
+int _putchar(char c);
 int _printf(const char *format, ...);
 int _print_a_char(va_list args);
 int _print_a_string(va_list args);
